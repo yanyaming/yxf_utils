2530e7d8fe05d2522247ae36d2a6803f1db14065
yanyaming <yanyaming4721@qq.com>
2018-11-07 15:20:31 +0800

    2018.11.07 modify

--- scripts/frp/frpc/start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#goto project root
-curpath=$(cd "$(dirname "$0")"; pwd)
-cd $curpath
-nohup ./frpc -c ./frpc.ini > frpc_console.log 2>&1 &
-exit 0
--- scripts/frp/frps/start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#goto project root
-curpath=$(cd "$(dirname "$0")"; pwd)
-cd $curpath
-nohup ./frps -c ./frps.ini > frps_console.log 2>&1 &
-exit 0
